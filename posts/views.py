8b063249c4986b41418a0e56b51d8e58a611cde7
Daisha <daisha05311994@gmail.com>
2024-07-13 14:23:33 -0400

    FSDIBLOG1

--- posts/views.py
+++ posts/views.py
@@ -1,4 +1,6 @@
-from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.urls import reverse_lazy
 from .models import Post
 
 
@@ -10,8 +12,29 @@ class PostDetailView(DetailView):
     template_name = "posts/detail.html"
     model = Post
 
-class PostCreateView(CreateView):
-    template_name = "posts/create.html"
+class PostCreateView(LoginRequiredMixin, CreateView):
+    template_name = "posts/new.html"
     model = Post
-    fields = ["title", "subtitle", "body"]
+    fields = ["title", "subtitle", "body", "status"]
 
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    template_name = "posts/edit.html"
+    model = Post
+    fields = ["title", "subtitle", "body", "status"]
+
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    template_name = "posts/delete.html"
+    model = Post
+    success_url = reverse_lazy("list")
+
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author
